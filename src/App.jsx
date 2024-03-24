b4370b0e3bf69adda2cd501bdeca5d13aaa39a10
Rafaella Silveira <rafaellasilveirass0@gmail.com>
2024-03-24 15:27:24 -0300

    instalação do tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
       <section>
         <div>
           <img src={iconSedans} alt="ícone dos carros sedan"/>
-          <h2>Sedans</h2>
+          <h2 className="text-3xl">Sedans</h2>
           <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
